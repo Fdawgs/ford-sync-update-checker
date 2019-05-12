e8f31b327bd873504c437fe605f9df957636a464
Frazer Smith <frazer.smith@ydh.nhs.uk>
2019-05-12 20:07:32 +0100

    Add PS script

---
+++ ford_sync_update_checker.ps1
@@ -0,0 +1,31 @@
+$url = "https://www.serviceseucache.ford.com/api/Sync/v1/getInstallPackages";
+$passMessage = "Already latest version has been installed for this VIN";
+
+$params = @{
+    locale = "en_GB";
+    vinCode = "TEST"; # Add own VIN for Ford vehicle here, will error if not valid VIN
+    appType = "map";
+}
+
+# Make HTTP GET request to check if Ford Maps is up to date
+try {
+    $mapRequest = Invoke-RestMethod $url -Method Get -Body $params;
+} catch {
+        Write-Host $_.Exception;
+        exit
+}
+
+# Make HTTP GET request to check if Ford SYNC is up to date
+$params."appType" = "sync";
+try {
+    $syncRequest = Invoke-RestMethod $url -Method Get -Body $params;
+} catch {
+    Write-Host $_.Exception;
+    exit
+}
+
+if ($($syncRequest.data.message) -ne $passMessage -or $($mapRequest.data.message) -ne $passMessage) {
+    Write-Output "Ford SYNC status: $($syncRequest.data.message)";
+    Write-Output "Ford Maps status: $($mapRequest.data.message)";
+    Read-Host "Press any key to continue ...";
+}
