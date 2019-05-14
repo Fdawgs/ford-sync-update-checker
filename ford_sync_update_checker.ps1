cf5ea2171f1cadca746dbad94bfc48c1ee5fdd55
Frazer Smith <frazer.smith@ydh.nhs.uk>
2019-05-14 18:56:31 +0100

    Replace double quotation marks with single
    looks cleaner!

--- ford_sync_update_checker.ps1
+++ ford_sync_update_checker.ps1
@@ -1,14 +1,14 @@
-$url = "https://www.serviceseucache.ford.com/api/Sync/v1/getInstallPackages";
-$passMessage = "Already latest version has been installed for this VIN";
+$url = 'https://www.serviceseucache.ford.com/api/Sync/v1/getInstallPackages';
+$passMessage = 'Already latest version has been installed for this VIN';
 
 $headers = @{
     'Upgrade-Insecure-Requests' = 1
 }
 
 $params = @{
-    locale = "en_GB";
-    vinCode = "TEST"; # Add own VIN for Ford vehicle here, will error if not valid VIN
-    appType = "map";
+    locale = 'en_GB';
+    vinCode = 'TEST'; # Add own VIN for Ford vehicle here, will error if not valid VIN
+    appType = 'map';
 }
 
 # Make HTTP GET request to check if Ford Maps is up to date
@@ -20,16 +20,16 @@ try {
 }
 
 # Make HTTP GET request to check if Ford SYNC is up to date
-$params."appType" = "sync";
+$params.'appType' = 'sync';
 try {
-    $syncRequest = Invoke-RestMethod $url -Method Get -Body $params;
+    $syncRequest = Invoke-RestMethod $url -Headers $headers -Method Get -Body $params;
 } catch {
     Write-Host $_.Exception;
     exit
 }
 
 if ($($syncRequest.data.message) -ne $passMessage -or $($mapRequest.data.message) -ne $passMessage) {
-    Write-Output "Ford SYNC status: $($syncRequest.data.message)";
-    Write-Output "Ford Maps status: $($mapRequest.data.message)";
-    Read-Host "Press any key to continue ...";
+    Write-Output 'Ford SYNC status: $($syncRequest.data.message)';
+    Write-Output 'Ford Maps status: $($mapRequest.data.message)';
+    Read-Host 'Press any key to continue ...';
 }
